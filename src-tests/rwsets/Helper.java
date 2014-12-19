1c2075c0372c1c86a0bb96bc3c2f73a756214c24
HoracioFilho <hjcf@cin.ufpe.br>
2014-12-19 00:16:23 -0200

    Torna os testes mais rápidos de serem produzidos

--- src-tests/rwsets/Helper.java
+++ src-tests/rwsets/Helper.java
@@ -7,18 +7,45 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class Helper {
-  
+  public static final String USER_DIR = System.getProperty("user.dir");
+  public static final String RESOURCES_DIR = USER_DIR + "/dat";
+  public static final String TEST_DIR = USER_DIR + "/src-tests";
+  public static final String EXCLUSION_FILE = RESOURCES_DIR + "/ExclusionAllJava.txt";
+  public static final String EXCLUSION_FILE_FOR_CALLGRAPH = RESOURCES_DIR + "/ExclusionForCallGraph.txt";
+  public static final String CLASS_NOT_FOUND = "A classe não foi encontrada";
+
   public static String readFile(String fileName) throws FileNotFoundException, IOException {
     StringBuffer sb = new StringBuffer();
+    
     FileReader fr = new FileReader(new File(fileName));
+    
     BufferedReader br = new BufferedReader(fr);
+    
     String tmp;
-    while ((tmp = br.readLine())!=null) {
+    
+    while ((tmp = br.readLine()) != null) {
       sb.append(tmp);
       sb.append("\n");
     }
+    
     br.close();
+    
     fr.close();
+    
     return sb.toString();
+  }
+  
+  private static String getCurrentTestName() {
+    Thread currentThread = Thread.currentThread();
+    
+    StackTraceElement[] stackTrace = currentThread.getStackTrace();
+    
+    String testName = stackTrace[2].getClassName().replace(".", "/") + "." + stackTrace[2].getMethodName();
+    
+    return testName;
+  }
+  
+  public static String getExpectedResultsFilePath() {
+    return String.format("%s/%s.data", TEST_DIR, getCurrentTestName());
   }
 }
