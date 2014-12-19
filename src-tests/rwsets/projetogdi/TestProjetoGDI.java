fe6a83bc45ece43a0bf0ed6997074dd6a97dbc8e
fulano7 <iagoraphael@gmail.com>
2014-12-19 12:57:10 -0300

    detalhe

--- src-tests/rwsets/projetogdi/TestProjetoGDI.java
+++ src-tests/rwsets/projetogdi/TestProjetoGDI.java
@@ -15,7 +15,6 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.sql.SQLException;
 
 import org.junit.Before;
 import org.junit.Test;
